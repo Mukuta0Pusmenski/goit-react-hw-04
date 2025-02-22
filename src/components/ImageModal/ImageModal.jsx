8547bc68898dbbce2b575c6b5ffd04bf8c6f2396
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-02-22 10:51:38 +0200

    push=3/4

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -9,9 +9,13 @@ const ImageModal = ({ isOpen, onRequestClose, image }) => {
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
-      maxWidth: '90%', // Обмежуємо максимальну ширину
-      maxHeight: '90%', // Обмежуємо максимальну висоту
-      overflow: 'hidden', // Додаємо прокрутку для великих зображень
+      width: '90%', // Обмежуємо максимальну ширину
+      height: '90%', // Обмежуємо максимальну висоту
+      overflow: 'hidden', // Запобігаємо прокрутці модального вікна
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'relative',
     },
     overlay: {
       backgroundColor: 'rgba(0, 0, 0, 0.75)', // Темний фон
@@ -30,11 +34,25 @@ const ImageModal = ({ isOpen, onRequestClose, image }) => {
       contentLabel="Image Modal"
       ariaHideApp={false}
     >
-      <button onClick={onRequestClose} style={{ position: 'absolute', top: 10, right: 10 }}>Close</button>
+      <button onClick={onRequestClose} style={{
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        backgroundColor: '#fff',
+        border: 'none',
+        padding: '5px 10px',
+        cursor: 'pointer',
+        borderRadius: '5px',
+        fontWeight: 'bold'
+      }}>Close</button>
       <img
         src={image.urls.full}
         alt={image.alt_description}
-        style={{ width: 'auto', height: 'auto', maxWidth: '100%', maxHeight: '100%' }}
+        style={{
+          maxWidth: '100%',
+          maxHeight: '100%',
+          objectFit: 'contain'
+        }}
       />
     </Modal>
   );
